9cf2be7bb81e7e32857298db196c1962996d50c9
LOSSY Roméo <rlossy@student.42.fr>
2020-03-05 11:43:40 +0100

    Had a crash when executing while a folder of same name already exists

--- C/Sul/Sully.c
+++ C/Sul/Sully.c
@@ -3,8 +3,8 @@
 #include <string.h>
 int main(){
 int i=5;
-char *path=getenv("_");if(!strstr(path,"./Sully_")){++i;}if(i-1<0){return(0);}
-char *s="#include <stdio.h>%4$c#include <stdlib.h>%4$c#include <string.h>%4$cint main(){%4$cint i=%3$d;%4$cchar *path=getenv(%2$c_%2$c);if(!strstr(path,%2$c./Sully_%2$c)){++i;}if(i-1<0){return(0);}%4$cchar *s=%2$c%5$s%2$c;%4$cchar *filename;char *compile;char *exe;FILE *f;%4$casprintf(&filename,%2$cSully_%1$cd.c%2$c,i-1);asprintf(&compile,%2$cgcc -Wall -Werror -Wextra Sully_%1$c1$d.c -o Sully_%1$c1$d; %2$c,i-1);asprintf(&exe,%2$c./Sully_%1$cd%2$c,i-1);%4$cf=fopen(filename,%2$cw%2$c);fprintf(f,s,37,34,i-1,10,s);fclose(f);system(compile);if(i>1){system(exe);}return(0);}";
+if(i<=0){return(0);}char *path=getenv("_");if(!strstr(path,"./Sully_")){++i;}
+char *s="#include <stdio.h>%4$c#include <stdlib.h>%4$c#include <string.h>%4$cint main(){%4$cint i=%3$d;%4$cif(i<=0){return(0);}char *path=getenv(%2$c_%2$c);if(!strstr(path,%2$c./Sully_%2$c)){++i;}%4$cchar *s=%2$c%5$s%2$c;%4$cchar *filename;char *compile;char *exe;FILE *f;%4$casprintf(&filename,%2$cSully_%1$cd.c%2$c,i-1);asprintf(&compile,%2$cgcc -Wall -Werror -Wextra Sully_%1$c1$d.c -o Sully_%1$c1$d; %2$c,i-1);asprintf(&exe,%2$c./Sully_%1$cd%2$c,i-1);%4$cif(!(f=fopen(filename,%2$cw%2$c))){return(1);}fprintf(f,s,37,34,i-1,10,s);fclose(f);system(compile);if(i>1){system(exe);}return(0);}";
 char *filename;char *compile;char *exe;FILE *f;
 asprintf(&filename,"Sully_%d.c",i-1);asprintf(&compile,"gcc -Wall -Werror -Wextra Sully_%1$d.c -o Sully_%1$d; ",i-1);asprintf(&exe,"./Sully_%d",i-1);
-f=fopen(filename,"w");fprintf(f,s,37,34,i-1,10,s);fclose(f);system(compile);if(i>1){system(exe);}return(0);}
+if(!(f=fopen(filename,"w"))){return(1);}fprintf(f,s,37,34,i-1,10,s);fclose(f);system(compile);if(i>1){system(exe);}return(0);}
